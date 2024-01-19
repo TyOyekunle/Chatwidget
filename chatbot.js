f993021d38108948e72b8a83b07ab92fd0b91f61
TyOyekunle <74738940+TyOyekunle@users.noreply.github.com>
2024-01-19 19:52:27 +0100

    I removed the script tag from the head to the body and added hover effect to the blue scroll button

--- chatbot.js
+++ chatbot.js
@@ -1,17 +1,4 @@
 
 
-function clickevent(){
-    alert('i clicked the logo')
-}
-document.getElementById('input-field').addEventListener('click', clickevent)
 
 
-// function leapYearChecker(year) {
-//     if (year % 4 === 0) {
-//         return `${year} is a leap year`
-//     } else {
-//         return `${year} is not a leap year`
-//     }
-// }
-// leapYearChecker()
-// console.log(leapYearChecker(2024))
